9f161f8be7b4729e15a41782327f8b58d2675098
Terran Whelan <terranwhelan@gmail.com>
2020-09-19 15:27:45 -0400

    found help with character code

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,26 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// generator functions
+
+//generates a random lower case from character code
+function getRandomLower(){
+return String.fromCharCode(Math.floor(Math.random() * 26 ) + 97);
+}
+console.log(getRandomLower());
+
+//generates random upper case from character code
+function getRandomUpper(){
+  return String.fromCharCode(Math.floor(Math.random() * 26 ) + 65)
+}
+console.log(getRandomUpper());
+
+//generates random number from character code
+function getRandomNumber(){
+  return String.fromCharCode(Math.floor(Math.random() * 10 ) + 48)
+}
+console.log(getRandomNumber());
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();

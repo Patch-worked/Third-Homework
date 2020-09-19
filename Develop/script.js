adb23c57084dec43fd1aee032002627fd51359e6
Terran Whelan <terranwhelan@gmail.com>
2020-09-19 17:47:49 -0400

    created a function to call a random function

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,25 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var passwordLength = prompt('how long would you like your password to be?' + "");
+if (8 > passwordLength || passwordLength > 128){
+  passwordLength = prompt('please enter a different number');
+    if(8 > passwordLength || passwordLength > 128){
+      prompt('default length is 12')
+      passwordLength = 12;
+    }
+}
+console.log(parseInt(passwordLength));
 
+// have to write an if statement for how many times to pick a random character
 // generator functions
+
+//function to pick a random function
+var functionsNeeded = [getRandomLower, getRandomUpper, getRandomNumber, getRandomSymbol];
+var n = functionsNeeded.length;
+function randomFunction(n){
+  return Math.floor( Math.random() * n);
+}
+
 
 //generates a random lower case from character code
 function getRandomLower(){
@@ -21,6 +39,12 @@ function getRandomNumber(){
 }
 console.log(getRandomNumber());
 
+function getRandomSymbol(){
+  const symbols = '!@#$%^&*(){}[]=<>/,.';
+  return symbols [Math.floor(Math.random() * symbols.length)];
+}
+console.log(getRandomSymbol());
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -32,3 +56,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+

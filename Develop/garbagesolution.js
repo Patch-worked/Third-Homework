b1142f9f7e363aba233430f2418c21b26745a7b5
Terran Whelan <terranwhelan@gmail.com>
2020-09-22 15:25:28 -0400

    Technically works, also have a wet solution, need to think of something else.

---
+++ Develop/garbagesolution.js
@@ -0,0 +1,236 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+var securePassword = document.querySelector('#placeholder');
+var passwordLength = prompt('how long would you like your password to be?' + "");
+var includeUppercase = confirm ("Would you like to include Uppercase Letters?")
+var includeLowercase = confirm ("Would you like to include Lowercase Letters?")
+var includeNumbers = confirm ("Would you like to include Numbers?")
+var includeSpecialCharacters = confirm ("Would you like to include Special Characters?")
+if (8 > passwordLength || passwordLength > 128){
+  passwordLength = prompt('please enter a different number');
+    if(8 > passwordLength || passwordLength > 128){
+      prompt('default length is 12')
+      passwordLength = 12;
+    }
+}
+console.log(parseInt(passwordLength));
+
+function generateP() { 
+    var pass = ''; 
+    var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' +  
+            'abcdefghijklmnopqrstuvwxyz0123456789@#$'; 
+      
+    for (i = 1; i <= passwordLength; i++) { 
+        var char = Math.floor(Math.random() 
+                    * str.length + 1); 
+          
+        pass += str.charAt(char) 
+        
+    } 
+    alert("your password is " + pass);
+    return pass;
+} 
+
+function generateAllupper() { 
+  var pass = ''; 
+  var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';  
+    
+  for (i = 1; i <= passwordLength; i++) { 
+      var char = Math.floor(Math.random() 
+                  * str.length + 1); 
+        
+      pass += str.charAt(char) 
+      
+  } 
+  alert("your password is " + pass);
+  return pass;
+} 
+
+function generateUpperLower() { 
+  var pass = ''; 
+  var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' +  
+          'abcdefghijklmnopqrstuvwxyz'; 
+    
+  for (i = 1; i <= passwordLength; i++) { 
+      var char = Math.floor(Math.random() 
+                  * str.length + 1); 
+        
+      pass += str.charAt(char) 
+      
+  } 
+  alert("your password is " + pass);
+  return pass;
+} 
+
+function generateUpperNumber() { 
+  var pass = ''; 
+  var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' +  
+          '0123456789'; 
+    
+  for (i = 1; i <= passwordLength; i++) { 
+      var char = Math.floor(Math.random() 
+                  * str.length + 1); 
+        
+      pass += str.charAt(char) 
+      
+  } 
+  alert("your password is " + pass);
+  return pass;
+} 
+
+function generateUpperSpecial() { 
+  var pass = ''; 
+  var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' +  
+          '@#$'; 
+    
+  for (i = 1; i <= passwordLength; i++) { 
+      var char = Math.floor(Math.random() 
+                  * str.length + 1); 
+        
+      pass += str.charAt(char) 
+      
+  } 
+  alert("your password is " + pass);
+  return pass;
+} 
+
+function generateUpperLowerNumber() { 
+  var pass = ''; 
+  var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' +  
+          'abcdefghijklmnopqrstuvwxyz0123456789'; 
+    
+  for (i = 1; i <= passwordLength; i++) { 
+      var char = Math.floor(Math.random() 
+                  * str.length + 1); 
+        
+      pass += str.charAt(char) 
+      
+  } 
+  alert("your password is " + pass);
+  return pass;
+} 
+
+function generateUpperLowerSpecial() { 
+  var pass = ''; 
+  var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' +  
+          'abcdefghijklmnopqrstuvwxyz@#$'; 
+    
+  for (i = 1; i <= passwordLength; i++) { 
+      var char = Math.floor(Math.random() 
+                  * str.length + 1); 
+        
+      pass += str.charAt(char) 
+      
+  } 
+  alert("your password is " + pass);
+  return pass;
+} 
+
+function generateLowerNumberSpecial() { 
+  var pass = ''; 
+  var str = 'abcdefghijklmnopqrstuvwxyz0123456789@#$'; 
+    
+  for (i = 1; i <= passwordLength; i++) { 
+      var char = Math.floor(Math.random() 
+                  * str.length + 1); 
+        
+      pass += str.charAt(char) 
+      
+  } 
+  alert("your password is " + pass);
+  return pass;
+} 
+
+function generateLowerNumber() { 
+  var pass = ''; 
+  var str = 'abcdefghijklmnopqrstuvwxyz0123456789'; 
+    
+  for (i = 1; i <= passwordLength; i++) { 
+      var char = Math.floor(Math.random() 
+                  * str.length + 1); 
+        
+      pass += str.charAt(char) 
+      
+  } 
+  alert("your password is " + pass);
+  return pass;
+} 
+
+function generateLowerSpecial() { 
+  var pass = ''; 
+  var str = 'abcdefghijklmnopqrstuvwxyz@#$'; 
+    
+  for (i = 1; i <= passwordLength; i++) { 
+      var char = Math.floor(Math.random() 
+                  * str.length + 1); 
+        
+      pass += str.charAt(char) 
+      
+  } 
+  alert("your password is " + pass);
+  return pass;
+} 
+
+function generateLower() { 
+  var pass = ''; 
+  var str ='abcdefghijklmnopqrstuvwxyz'; 
+    
+  for (i = 1; i <= passwordLength; i++) { 
+      var char = Math.floor(Math.random() 
+                  * str.length + 1); 
+        
+      pass += str.charAt(char) 
+      
+  } 
+  alert("your password is " + pass);
+  return pass;
+} 
+
+function generateNumber() { 
+  var pass = ''; 
+  var str = '0123456789'; 
+    
+  for (i = 1; i <= passwordLength; i++) { 
+      var char = Math.floor(Math.random() 
+                  * str.length + 1); 
+        
+      pass += str.charAt(char) 
+      
+  } 
+  alert("your password is " + pass);
+  return pass;
+} 
+
+function generateNumberSpecial() { 
+  var pass = ''; 
+  var str = '0123456789@#$'; 
+    
+  for (i = 1; i <= passwordLength; i++) { 
+      var char = Math.floor(Math.random() 
+                  * str.length + 1); 
+        
+      pass += str.charAt(char) 
+      
+  } 
+  alert("your password is " + pass);
+  return pass;
+} 
+
+function generateSpecial() { 
+  var pass = ''; 
+  var str = '@#$'; 
+    
+  for (i = 1; i <= passwordLength; i++) { 
+      var char = Math.floor(Math.random() 
+                  * str.length + 1); 
+        
+      pass += str.charAt(char) 
+      
+  } 
+  alert("your password is " + pass);
+  return pass;
+} 
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", generateP);
+
--- Develop/script2.js
+++ Develop/script2.js
@@ -26,15 +26,14 @@ function generateP() {
     return pass;
 } 
 
+ // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+//   passwordText.value = password;
 
-  passwordText.value = password;
-
-}
+// }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", generateP);
